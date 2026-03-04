88b346f9eee202f3f141470b72e5b38216b0dc8b
Renat <rinotello@yandex.ru>
2026-03-04 15:31:58 +0300

    import-models

--- src/script.js
+++ src/script.js
@@ -1,143 +1,64 @@
-import * as THREE from 'three';
-import {OrbitControls} from  'three/examples/jsm/controls/OrbitControls.js';
-import * as dat from 'lil-gui';
-import gsap from 'gsap';
-import * as CANNON from 'cannon-es';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import * as dat from "lil-gui";
+import gsap from "gsap";
+import * as CANNON from "cannon-es";
 
 // Debug
 const gui = new dat.GUI();
-const debugObject = {};
-debugObject.createSphere = () => {
-    createSphere(
-        Math.random() * 0.5, 
-        {
-            x: (Math.random() - 0.5) * 3, 
-            y: 3, 
-            z: (Math.random() - 0.5) * 3 
-        })
-}
-debugObject.createBox = () => {
-    createBox(
-        Math.random(),
-        Math.random(),
-        Math.random(), 
-        {
-            x: (Math.random() - 0.5) * 3, 
-            y: 3, 
-            z: (Math.random() - 0.5) * 3 
-        })
-}
-debugObject.reset = () => {
-    for(const object of objectsToUpdate) {
-        // Remove body
-        object.body.removeEventListener('collide', playSound);
-        world.remove(object.body);
 
-        // Remove mesh
-        scene.remove(object.mesh);
-
-        objectsToUpdate.splice(0, objectsToUpdate.length);
-    }
-}
-
-gui.add(debugObject,'createSphere');
-gui.add(debugObject,'createBox');
-gui.add(debugObject,'reset');
-
-const canvas = document.getElementById('webgl');
+const canvas = document.getElementById("webgl");
 // Cursor
 const cursor = {
-    x: 0,
-    y: 0,
-}
+  x: 0,
+  y: 0,
+};
 
 // Scene
 const scene = new THREE.Scene();
 
-// Sounds
-const hitSound = new Audio('/sounds/hit2.mp3');
-const playSound = (collision) => {
-    const impactStrength = collision.contact.getImpactVelocityAlongNormal();
+// Models
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/");
 
-    if(impactStrength > 1.5) {
-        hitSound.volume = Math.random();
-        hitSound.currentTime = 0;
-        hitSound.play();
-    }
-}
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
 
-// Textures
-const cubeTextureLoader = new THREE.CubeTextureLoader();
+let mixer = null;
 
-const environmentMapTexture = cubeTextureLoader.load([
-    '/textures/environmenMaps/0/px.jpg',
-    '/textures/environmenMaps/0/nx.jpg',
-    '/textures/environmenMaps/0/py.jpg',
-    '/textures/environmenMaps/0/ny.jpg',
-    '/textures/environmenMaps/0/pz.jpg',
-    '/textures/environmenMaps/0/nz.jpg',
-]);
+gltfLoader.load("/models/Fox/glTF/Fox.gltf", (gltf) => {
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  const action = mixer.clipAction(gltf.animations[2]);
 
-// Physics
+  action.play();
 
-// Physic World
-const world = new CANNON.World();
-world.broadphase = new CANNON.SAPBroadphase(world);
-world.allowSleep = true;
-world.gravity.set(0, - 9.82, 0);
-
-// Physics material
-const defaultMaterial = new CANNON.Material('default');
-
-const defaultContactMaterial = new CANNON.ContactMaterial(
-    defaultMaterial,
-    defaultMaterial,
-    {
-        friction: 0.1,
-        restitution: 0.7
-    }
-);
-world.addContactMaterial(defaultContactMaterial);
-world.defaultContactMaterial = defaultContactMaterial;
-
-
-
-// Physic floor
-const floorShape = new CANNON.Plane();
-const floorBody = new CANNON.Body({
-    mass: 0,
-    shape: floorShape,
+  gltf.scene.scale.set(0.025, 0.025, 0.025);
+  scene.add(gltf.scene);
 });
-floorBody.quaternion.setFromAxisAngle(
-    new CANNON.Vec3(-1, 0, 0),
-    Math.PI * 0.5
-);
-world.addBody(floorBody);
-
-
 
 // Floor
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#777777',
-        metalness: 0.3,
-        roughness: 0.4,
-        envMap: environmentMapTexture
-    })
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#777777",
+    metalness: 0.3,
+    roughness: 0.4,
+  }),
 );
 floor.receiveShadow = true;
-floor.rotation.x = - Math.PI * 0.5;
+floor.rotation.x = -Math.PI * 0.5;
 scene.add(floor);
 
 // Lights
 
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambientLight);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 0.5);
+const directionalLight = new THREE.DirectionalLight("#ffffff", 0.5);
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.camera.far = 15;
@@ -150,28 +71,28 @@ scene.add(directionalLight);
 
 // Sizes
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('mousemove', (e) => {
-    cursor.x = e.clientX / sizes.width - 0.5;
-    cursor.y = e.clientY / sizes.height - 0.5;
-})
+window.addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = e.clientY / sizes.height - 0.5;
+});
 
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    //Update renderer
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-})
+  //Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 // Обработка двойного клика
 // window.addEventListener('dblclick', () => {
@@ -192,7 +113,12 @@ window.addEventListener('resize', () => {
 // })
 
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
 camera.position.set(-5, 5, 3);
 scene.add(camera);
 
@@ -202,120 +128,36 @@ controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 });
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Utils
-const objectsToUpdate = [];
-
-// Sphere
-const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
-const sphereMaterial = new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture
-});
-
-const createSphere = (radius, position) => {
-    // Three.js mesh
-    const mesh = new THREE.Mesh(
-        sphereGeometry,
-        sphereMaterial
-    )
-    mesh.scale.set(radius, radius, radius);
-    mesh.castShadow = true;
-    mesh.position.copy(position);
-    scene.add(mesh);
-
-    // Cannon.js body
-    const shape = new CANNON.Sphere(radius);
-    const body = new CANNON.Body({
-        mass: 1,
-        position: new CANNON.Vec3(0, 3, 0),
-        shape,
-        material: defaultMaterial
-    });
-    body.position.copy(position);
-    body.addEventListener('collide', playSound);
-    world.addBody(body);
-
-    // Save in objects to update
-    objectsToUpdate.push({
-        mesh,
-        body
-    })
-}
-
-createSphere(0.5, { x: 0, y: 3, z: 0 });
-
-// Box
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture
-});
-
-const createBox = (width, height, depth, position) => {
-    // Three.js mesh
-    const mesh = new THREE.Mesh(
-        boxGeometry,
-        boxMaterial
-    )
-    mesh.scale.set(width, height, depth);
-    mesh.castShadow = true;
-    mesh.position.copy(position);
-    scene.add(mesh);
-
-    // Cannon.js body
-    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5));
-    const body = new CANNON.Body({
-        mass: 1,
-        position: new CANNON.Vec3(0, 3, 0),
-        shape,
-        material: defaultMaterial
-    });
-    body.position.copy(position);
-    body.addEventListener('collide', playSound);
-    world.addBody(body);
-
-    // Save in objects to update
-    objectsToUpdate.push({
-        mesh,
-        body
-    })
-}
-
 // Clock
 const clock = new THREE.Clock();
-let oldElapsedTime = 0;
+let previousTime = 0;
 
 // Animations
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-    const deltaTime = elapsedTime - oldElapsedTime;
-    oldElapsedTime = elapsedTime;
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
-    // Update physics world
-    world.step(1 / 60, deltaTime, 3);
+  // Update mixer
+  if (mixer !== null) {
+    mixer.update(deltaTime);
+  }
 
-    for(const object of objectsToUpdate) {
-        object.mesh.position.copy(object.body.position);
-        object.mesh.quaternion.copy(object.body.quaternion);
-    }
+  // Update controls
+  controls.update();
 
-    // Update controls
-    controls.update();
-    
-    // Render
-    renderer.render(scene, camera);
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
 };
 
 tick();
