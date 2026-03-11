91af16de3bb1ac47718e3baa097b74d8ea3dbb94
Renat <rinotello@yandex.ru>
2026-03-11 12:13:53 +0300

    shader-templates

--- src/script.js
+++ src/script.js
@@ -25,52 +25,15 @@ const scene = new THREE.Scene();
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
-const count = geometry.attributes.position.count;
-const randoms = new Float32Array(count);
-
-for (let i = 0; i < count; i++) {
-  randoms[i] = Math.random();
-}
-
-geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
-
 // Material
 const material = new THREE.ShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
-  uniforms: {
-    uFrequency: {
-      value: new THREE.Vector2(10, 5),
-    },
-    uTime: {
-      value: 0,
-    },
-    uColor: {
-      value: new THREE.Color("blue"),
-    },
-    uTexture: {
-      value: flagTexture,
-    },
-  },
+  side: THREE.DoubleSide,
 });
-
-gui
-  .add(material.uniforms.uFrequency.value, "x")
-  .min(0)
-  .max(20)
-  .step(0.01)
-  .name("frequencyX");
-
-gui
-  .add(material.uniforms.uFrequency.value, "y")
-  .min(0)
-  .max(20)
-  .step(0.01)
-  .name("frequencyY");
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
-mesh.scale.y = 2 / 3;
 scene.add(mesh);
 
 // Sizes
@@ -121,14 +84,10 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Clock
 const clock = new THREE.Clock();
-let previousTime = 0;
 
 // Animations
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Update material
-  material.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
